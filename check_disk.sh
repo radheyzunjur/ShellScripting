4efcdae11f77091c03624c50a4a4badebeb3df9e
Radhey Zunjur <72388543+radheyzunjur@users.noreply.github.com>
2023-06-23 10:09:16 +0530

    Update check_disk.sh

--- check_disk.sh
+++ check_disk.sh
@@ -7,7 +7,7 @@ do
  usage=$(echo $output | awk '{print $1}' | cut -d'%' -f1)
  file_sys=$(echo $output |awk '{print $2}')
  echo $usage
- if [$usage -ge 90]
+ if ["$usage" -ge 90];
  then
  	echo "CRITICAL for $file_sys"
 fi
